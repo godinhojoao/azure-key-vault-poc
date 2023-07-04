88a77058b6b7df301503abd1742aab962a5fb61d
João Godinho Jardim <godinhojoao@MacBook-Air-de-Joao.local>
2023-07-04 12:29:33 -0300

    feat poc nodejs azure key vault

--- index.js
+++ index.js
@@ -1,11 +1,53 @@
-const express = require('express');
-const app = express();
-const port = process.env.PORT || 3000;
+const { DefaultAzureCredential } = require("@azure/identity");
+const { SecretClient } = require("@azure/keyvault-secrets");
+require("dotenv").config();
 
-app.get('/hello', (req, res) => {
-  res.send('Hello, world!');
+const credential = new DefaultAzureCredential({
+  tenantId: process.env["AZURE_TENANT_ID"],
+  clientId: process.env["AZURE_CLIENT_ID"],
 });
+const keyVaultName = process.env["KEY_VAULT_NAME"];
+if (!keyVaultName) {
+  throw new Error("KEY_VAULT_NAME is empty");
+}
 
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
+const url = "https://" + keyVaultName + ".vault.azure.net";
+const client = new SecretClient(url, credential);
+
+(async function () {
+  try {
+    const secret = await client.getSecret(process.env["SECRET_KEY_NAME"]);
+    console.log('secret', secret)
+  } catch (error) {
+    console.error("Error retrieving secret:", error.message);
+  }
+})()
+
+
+// code to use with node v12
+// const { ClientSecretCredential } = require("@azure/identity");
+// const { SecretClient } = require("@azure/keyvault-secrets");
+// require("dotenv").config();
+
+// const credential = new ClientSecretCredential({
+//   tenantId: process.env.AZURE_TENANT_ID,
+//   clientId: process.env.AZURE_CLIENT_ID,
+//   clientSecret: process.env.AZURE_CLIENT_SECRET,
+// });
+
+// const keyVaultName = process.env.KEY_VAULT_NAME;
+// if (!keyVaultName) {
+//   throw new Error("KEY_VAULT_NAME is empty");
+// }
+
+// const url = `https://${keyVaultName}.vault.azure.net`;
+// const client = new SecretClient(url, credential);
+
+// (async function () {
+//   try {
+//     const secret = await client.getSecret(process.env.SECRET_KEY_NAME);
+//     console.log('secret', secret);
+//   } catch (error) {
+//     console.error("Error retrieving secret:", error.message);
+//   }
+// })();
---
+++ readme.md
@@ -0,0 +1,8 @@
+# Azure key vault + Nodejs poc
+
+## First you need to create a key vault on your azure account.
+
+## Steps to use Azure Key Vault
+1 - `create a .env following the .env.example`
+2 - `az login`
+3 - `npm run start`
